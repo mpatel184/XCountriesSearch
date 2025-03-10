5eb0ed7e354c6f9663740e5768cbdc86251ff86c
mpatel184 <mvekariya398@gmail.com>
2025-03-10 20:30:50 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from "axios"
+import "tailwindcss"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [countries, setCountries] = useState([])
+  const [searchTerm, setSearchTerm] = useState('');
+  const fetchData = async() => {
+    try {
+      const response = await axios.get("https://countries-search-data-prod-812920491762.asia-south1.run.app/countries")
+      setCountries(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const filteredCountries = countries.filter((country) =>
+    country.common.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  
+  useEffect(() => {
+    fetchData()
+  },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="mb-6">
+        <input
+          type="text"
+          placeholder="Search for countries..."
+          className="w-full p-2 border border-gray-300 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="grid grid-cols-5 gap-4">
+        {filteredCountries.map((country, index) => {
+          const { common, png } = country;
+          return (
+            <div
+              key={index}
+              className="p-4 border border-gray-300 rounded-md shadow-md flex flex-col items-center"
+            >
+              <img
+                src={png}
+                alt={common}
+                className="w-20 h-14 object-cover mb-2 border rounded-md"
+              />
+              <h2 className="text-lg font-semibold">{common}</h2>
+            </div>
+          );
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
